31367d2a192ad5e1b55c5c7ae9a3b41843ffe731
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-11 17:39:03 +0100

    problemi

--- js/script.js
+++ js/script.js
@@ -2,24 +2,31 @@ console.log('vue ok', Vue)
 
 const app = Vue.createApp({
     data(){
-        tusks= [
-            {
-                task: 'Fare la spesa',
-                done: 'folse'
-            },
-            {
-                task: 'Lavare la macchina',
-                done: 'folse'
-            },
-            {
-                task: 'Fare il ragù',
-                done: 'folse'
-            },
-            {
-                task: 'Mettere apposto le legne',
-                done: 'folse'
-            },
-        ]
+        return{
+            tasks: [
+                {
+                    task: 'Fare la spesa',
+                    done: 'folse'
+                },
+                {
+                    task: 'Lavare la macchina',
+                    done: 'folse'
+                },
+                {
+                    task: 'Fare il ragù',
+                    done: 'folse'
+                },
+                {
+                    task: 'Mettere apposto le legne',
+                    done: 'folse'
+                },
+            ]
+        }
+    },
+    methods: {
+        deed(){
+            this.done = 'true';
+        }
     }
 });
 
