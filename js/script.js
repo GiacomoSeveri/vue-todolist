678795351fa3a2673a65a9f73fa85f920d4b3f58
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-11 17:16:05 +0100

    added array and html

--- js/script.js
+++ js/script.js
@@ -2,7 +2,24 @@ console.log('vue ok', Vue)
 
 const app = Vue.createApp({
     data(){
-
+        tusks= [
+            {
+                task: 'Fare la spesa',
+                done: 'folse'
+            },
+            {
+                task: 'Lavare la macchina',
+                done: 'folse'
+            },
+            {
+                task: 'Fare il ragù',
+                done: 'folse'
+            },
+            {
+                task: 'Mettere apposto le legne',
+                done: 'folse'
+            },
+        ]
     }
 });
 
